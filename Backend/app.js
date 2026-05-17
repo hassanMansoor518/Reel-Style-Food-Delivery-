8a5f06b876009ec4ca9e146bd94631b1a9de3e7f
HassanMansoor <hassanmansoor518@gmail.com>
2026-05-17 19:19:42 +0500

    feat: initialize Express application with middleware and API routes

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ const cors = require("cors")
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:5173", "https://reel-style-food-delivery-cmeg.vercel.app"],
+  origin: ["http://localhost:5173", "https://reel-style-food-delivery-cmeg.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }))
